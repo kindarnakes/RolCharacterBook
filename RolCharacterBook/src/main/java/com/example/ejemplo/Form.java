090c4f728de9aa756c428f463152fe3d9b91ecca
kindarnakes <kindenene@gmail.com>
2020-11-15 20:34:47 +0100

    Initial commit

---
+++ RolCharacterBook/src/main/java/com/example/ejemplo/Form.java
@@ -0,0 +1,28 @@
+package com.example.ejemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        String[] arraySpinner = new String[] {
+                "Bárbaro", "Guerrero", "Mago", "Clerigo"
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
+    public void onBackPressed(View view) {
+        onBackPressed();
+    }
+}
---
+++ RolCharacterBook/src/main/java/com/example/ejemplo/List.java
@@ -0,0 +1,36 @@
+package com.example.ejemplo;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(List.this, Form.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onBackPressed(View view) {
+        finish();
+    }
+}
